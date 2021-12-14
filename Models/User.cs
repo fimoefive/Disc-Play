fcd093610a413a4cc88ffaded308cceaf47c00ea
Martin Sisk <Brothermj@hotmail.com>
2021-12-14 16:34:36 -0600

    Created all the User Properties in the Models folder for User.cs file.

--- Models/User.cs
+++ Models/User.cs
@@ -7,5 +7,12 @@ namespace Disc_Play.Models
 {
   public class User
   {
+    //	User Properties
+    public int UserID { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string UID { get; set; }
+    public string UserRole { get; set; }
   }
 }
